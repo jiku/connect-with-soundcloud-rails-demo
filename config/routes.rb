d21ae4130ec38785fd2af80bf3930fd3a8b553d6
B.E. Henriksen <e@jiku.ca>
2013-12-13 12:50:59 +0100

    Updates app to Rails 4.0.2 and soundcloud-ruby 0.3.2.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 ConnectWithSoundCloudDemo::Application.routes.draw do
   root :to => "welcome#index"
 
-  match '/you',           :to => 'welcome#show',    :as => :you
-  match '/you/update',    :to => 'welcome#update',  :as => :you_update
+  get '/you',           :to => 'welcome#show',    :as => :you
+  get '/you/update',    :to => 'welcome#update',  :as => :you_update
 
-  match '/soundcloud/connect',    :to => 'soundcloud#connect',    :as => :soundcloud_connect
-  match '/soundcloud/connected',  :to => 'soundcloud#connected',  :as => :soundcloud_connected
-  match '/soundcloud/disconnect', :to => 'soundcloud#disconnect', :as => :soundcloud_disconnect
+  get '/soundcloud/connect',    :to => 'soundcloud#connect',    :as => :soundcloud_connect
+  get '/soundcloud/connected',  :to => 'soundcloud#connected',  :as => :soundcloud_connected
+  get '/soundcloud/disconnect', :to => 'soundcloud#disconnect', :as => :soundcloud_disconnect
 
   # See how all your routes lay out with "rake routes"
 
